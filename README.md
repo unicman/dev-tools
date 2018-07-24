9e8c0a652bddacc7145e2d57c5cc44f3494057b7
unicman <unicman@gmail.com>
2018-07-24 12:57:26 +0530

    Updated script for setting up screen tool and readme for it.

--- README.md
+++ README.md
@@ -8,3 +8,10 @@ curl https://raw.githubusercontent.com/unicman/dev-tools/master/setup-vim.sh | b
 ```
 
 This should install pathogen and useful VIM plugins, syntax files and compilers. It should work for VIM 7.4+ ... best is if you have VIM 8.0+ because some cool plugins like ALE need that version.
+
+## Setup screen tool (Only for Linux)
+
+```
+curl https://raw.githubusercontent.com/unicman/dev-tools/master/setup-screen.sh | bash
+```
+
--- setup-screen.sh
+++ setup-screen.sh
@@ -19,23 +19,55 @@ fnExec()
     $*
 }
 
+fnGitClone()
+{
+	GIT_URL=$1
+	DIR_PLUGIN=$2
+	if [ -d ${DIR_PLUGIN} ] ; then
+		fnExec cd ${DIR_PLUGIN}
+		fnExec git pull
+	else
+		fnExec git clone ${GIT_URL} ${DIR_PLUGIN}
+	fi
+}
+
 ########################################
 # Setup logic
 ########################################
 DIR_UM_GIT=~/um-git
 UNAME_OS=$(uname -o)
 UNAME=$(uname)
-STEPS=6
+STEPS=2
 STEP=0
 
 if [ "${TMP}" == "" ] ; then
 	export TMP="/tmp"
 fi
 
-if [ "$UNAME" != "Linux" ] ; then
-        echo "This script is only supported on Linux."
-        exit 1
+if [ "$UNAME_OS" == "Msys" ] ; then
+    echo "Screen setup is not applicable on Windows. Skipping."
+    exit 0
+elif [ "$UNAME" != "Linux" ] ; then
+    echo "This script is only supported on Linux."
+    exit 1
 fi
+
+STEP=`expr ${STEP} + 1`
+echo ""
+echo "********************************************************************************"
+echo "**** Step ${STEP}/${STEPS} Download all setup files from github..."
+echo "********************************************************************************"
+echo ""
+
+fnExec mkdir -p ${DIR_UM_GIT} # Create folder in user home to fetch all setup files
+fnGitClone https://github.com/unicman/dev-tools.git ${DIR_UM_GIT}/dev-tools # Fetch all setup files
+
+STEP=`expr ${STEP} + 1`
+echo ""
+echo "********************************************************************************"
+echo "**** Step ${STEP}/${STEPS} Linking screen utility config file..."
+echo "********************************************************************************"
+echo ""
 
 fnExec ln -sf ${DIR_UM_GIT}/dev-tools/screenrc ~/.screenrc
 
