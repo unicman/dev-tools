617f78f7d9f262dcab64c26fdcbc2698cfdaeae6
unicman <unicman@gmail.com>
2021-01-13 11:27:23 +0530

    Updated to work even if no containers are running.

--- bash-aliases.sh
+++ bash-aliases.sh
@@ -5,6 +5,6 @@
 # 06/29/2020           unicman         Created
 ################################################################################
 
-alias tools.docker-clean-all='echo "Deleting exited containers..." && docker rm -v $(docker ps -a -q -f status=exited) && echo "Deleting untagged images." && docker rmi $(docker images -f "dangling=true" -q) && echo "SUCCESS."'
+alias tools.docker-clean-all='echo "Deleting exited containers..." && (docker rm -v $(docker ps -a -q -f status=exited) || true) && echo "Deleting untagged images." && docker rmi $(docker images -f "dangling=true" -q) && echo "SUCCESS."'
 
 alias tools.reload-ssh='echo "Removing PKCS cached SSH key..." && ssh-add -e /usr/local/lib/opensc-pkcs11.so && echo "Adding PKCS SSH key..." && ssh-add -s /usr/local/lib/opensc-pkcs11.so'
