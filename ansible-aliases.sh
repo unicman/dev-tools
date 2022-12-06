9eabfc12b9f57737b686c4b6a7eed50e1d4d7e1f
unicman <unicman@gmail.com>
2022-12-06 17:15:26 +0530

    Little more optimised host lists fetch logic.

--- ansible-aliases.sh
+++ ansible-aliases.sh
@@ -7,7 +7,8 @@ fnSshAnsibleConfigAutocomplete() {
     COMPREPLY=()
     cur="${COMP_WORDS[COMP_CWORD]}"
     prev="${COMP_WORDS[COMP_CWORD-1]}"
-    opts=$(ansible-inventory --graph 2>&1 | grep "|--[^@]" | sed 's/^.*--//g' | sort | uniq)
+    #opts=$(ansible-inventory --graph 2>&1 | grep "|--[^@]" | sed 's/^.*--//g' | sort | uniq)
+    opts=$(ansible-inventory --list 2> /dev/null | jq -r '.[] | .hosts | .[]' 2> /dev/null)
 
     COMPREPLY=( $(compgen -W "$opts" -- ${cur}) )
     return 0
