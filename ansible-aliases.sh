a9929b758c1fd182e7ad2c547ccf83709d89e281
unicman <unicman@gmail.com>
2023-02-14 14:47:09 +0530

    Cache ansible inventory for a day.

--- ansible-aliases.sh
+++ ansible-aliases.sh
@@ -8,7 +8,13 @@ fnSshAnsibleConfigAutocomplete() {
     cur="${COMP_WORDS[COMP_CWORD]}"
     prev="${COMP_WORDS[COMP_CWORD-1]}"
     #opts=$(ansible-inventory --graph 2>&1 | grep "|--[^@]" | sed 's/^.*--//g' | sort | uniq)
-    opts=$(ansible-inventory --list 2> /dev/null | jq -r '.[] | .hosts | .[]' 2> /dev/null)
+    chh=$(find /tmp/bash-ansible-cache -mtime +1 -print 2> /dev/null || true)
+    if [ -f /tmp/bash-ansible-cache ] && [ "${chh}" != "/tmp/bash-ansible-cache" ] ; then
+        opts=$(cat /tmp/bash-ansible-cache)
+    else
+        opts=$(ansible-inventory --list 2> /dev/null | jq -r '.[] | .hosts | .[]' 2> /dev/null)
+        echo "${opts}" > /tmp/bash-ansible-cache
+    fi
 
     COMPREPLY=( $(compgen -W "$opts" -- ${cur}) )
     return 0
