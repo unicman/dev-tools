30747d56e99336d8ffa8152fa0baed50e840c8a2
unicman <unicman@gmail.com>
2023-03-10 17:00:13 +0530

    Latest pmset config.

--- bash-aliases.sh
+++ bash-aliases.sh
@@ -2,6 +2,8 @@
 ################################################################################
 # File: bash-aliases.sh
 #
+# There is a speling mistake.
+# 
 # Date (M/D/Y)         Name            Description
 # 06/29/2020           unicman         Created
 ################################################################################
@@ -72,9 +74,9 @@ alias terraform.filter-graph='__fnTerraformFilterGraph'
 alias pmset.list='pmset -g'
 
 
-alias pmset.set.laptop-working='sudo pmset -a lidwake 1 && sudo pmset -b sleep 15 displaysleep 5 powernap 0'
-alias pmset.set.docked='sudo pmset -a lidwake 0 displaysleep 300 acwake 1'
-alias pmset.set.presentation='sudo pmset -a lidwake 0 displaysleep 0 sleep 0 acwake 0'
+alias pmset.set.laptop-working='sudo pmset -a lidwake 1 standby 0 && sudo pmset -b sleep 15 displaysleep 5 powernap 0'
+alias pmset.set.docked='sudo pmset -a lidwake 0 displaysleep 300 acwake 1 powernap 1 standby 0'
+alias pmset.set.presentation='sudo pmset -a lidwake 0 displaysleep 0 sleep 0 acwake 0 standby 0'
 
 alias cpu-temperature='sudo powermetrics --samplers smc |grep -i "CPU die temperature"'
 
