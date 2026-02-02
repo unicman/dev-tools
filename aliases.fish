670f6cecc27d63034895836bbf76f7e28d8ce87a
unicman <unicman@gmail.com>
2026-02-02 11:01:44 +0000

    Install terraform and java tool versions if configured in current folder.

--- aliases.fish
+++ aliases.fish
@@ -1,11 +1,31 @@
-fish_add_path (dirname (realpath (status -f)))/shell-scripts
+fish_add_path -a (dirname (realpath (status -f)))/shell-scripts
+
+if test -e /opt/homebrew/bin
+    fish_add_path -p /opt/homebrew/bin
+end
 
 source (dirname (realpath (status -f)))/aliases.sh.fish
 
+# Load fish script if present in current folder
 if test -e ./.fishrc
    source ./.fishrc
 end
 
+# Load aliases if present in current folder
 if test -e ./aliases.sh.fish 
     source ./aliases.sh.fish
+end
+
+# Load terraform vesion if specified for current folder
+if test -e ./.terraform-version
+    if command -q tfenv
+        tfenv use
+    end
+end
+
+# Load vesions of java tools if specified for current folder
+if test -e ./.sdkmanrc
+    if command -q sdk
+        sdk env use
+    end
 end
